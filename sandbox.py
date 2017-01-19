bb6878ad27caa344c3be83d725559b827f9f7c4a
benjaponum <benjaponum@gmail.com>
2017-01-20 07:32:50 +0900

    added border to the button

--- sandbox.py
+++ sandbox.py
@@ -2,12 +2,15 @@ import ui
 
 
 def button_tapped(sender):
-    sender.text = "Tapped!"
+    sender.title = "Tapped!"
+    sender.size_to_fit()
 
 view = ui.View()
 view.background_color = 'white'
 button = ui.Button(title='Button')
 button.font = ('<system>', 40)
+button.border_width = 1
+button.border_color = "black"
 button.size_to_fit()
 button.center = (view.width*0.5, view.height*0.5) 
 button.flex = 'LRTB'
