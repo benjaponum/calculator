7e74601e39da7c208499d3f06b82d2914cef6ff7
benjaponum <benjaponum@gmail.com>
2017-01-20 07:20:21 +0900

    added action to the button

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,9 @@
 import ui
 
+
+def button_tapped(sender):
+    sender.text = "Tapped!"
+
 view = ui.View()
 view.background_color = 'white'
 button = ui.Button(title='Button')
@@ -7,5 +11,6 @@ button.font = ('<system>', 40)
 button.size_to_fit()
 button.center = (view.width*0.5, view.height*0.5) 
 button.flex = 'LRTB'
+button.action = button_tapped
 view.add_subview(button)
 view.present()
